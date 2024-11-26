46ad9cda23b61ed99332b23f6f38bfd985a1c742
k3310 <187008159+k3310@users.noreply.github.com>
2024-11-26 03:14:11 +0000

    [Ado] booksのshow-view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,17 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params) 
     @book.user.id = current_user.id
-    @book.save
-    redirect_to book_path
+    if @book.save
+      redirect_to book_path
+    else
+      render :index
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
